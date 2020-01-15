b8fc2afc92516a53e90c1938303324f54f4a822d
Justin Lovinger <git@justinlovinger.com>
2020-01-15 18:41:52 -0500

    Add project file paths
    for later hosting.
    URLs are duplicated
    because I'm not sure how to
    effectively reuse the same string
    in Nix and PureScript.

---
+++ files.nix
@@ -0,0 +1,12 @@
+{
+  projects = {
+    labyrinth-rl = {
+      file = ~/data/_-_file/by-name/labyrinth-rl-1_0_0.zip;
+      url = "/file/labyrinth-rl.zip";
+    };
+    gladiator-manager = {
+      file = ~/data/_-_file/by-name/gladiator-manager-1_0_0.zip;
+      url = "/file/gladiator-manager.zip";
+    };
+  };
+}
