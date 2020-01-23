3e7471b73e03fdce74cfcd434229f0feab846aa4
Justin Lovinger <git@justinlovinger.com>
2020-01-23 16:29:51 -0500

    Add papers data

--- files.nix
+++ files.nix
@@ -1,12 +1,46 @@
 {
-  projects = {
-    labyrinth-rl = {
-      file = ~/data/_-_file/by-name/labyrinth-rl-1_0_0.zip;
-      url = "/file/labyrinth-rl.zip";
+  papers = {
+    gist = {
+      file = ~/data/_-_file/by-name/gist.pdf;
+      url = "/file/gist.pdf";
     };
+    harnessing-mother-nature = {
+      file = ~/data/_-_file/by-name/harnessing-mother-nature.pdf;
+      url = "/file/harnessing-mother-nature.pdf";
+    };
+    infinite-lattice-learner = {
+      file = ~/data/_-_file/by-name/infinite-lattice-learner.pdf;
+      url = "/file/infinite-lattice-learner.pdf";
+    };
+    neural-field = {
+      file = ~/data/_-_file/by-name/neural-field.pdf;
+      url = "/file/neural-field.pdf";
+    };
+    scrubbing-the-web-for-association-rules = {
+      file = ~/data/_-_file/by-name/scrubbing-the-web-for-association-rules.pdf;
+      url = "/file/scrubbing-the-web-for-association-rules.pdf";
+    };
+    sma-star-plus = {
+      file = ~/data/_-_file/by-name/sma-star-plus.pdf;
+      url = "/file/sma-star-plus.pdf";
+    };
+    supervised-learning-as-mathematical-optimization-tutorial = {
+      file = ~/data/_-_file/by-name/supervised-learning-as-mathematical-optimization-tutorial.pdf;
+      url = "/file/supervised-learning-as-mathematical-optimization-tutorial.pdf";
+    };
+    the-effect-of-human-thought-on-data = {
+      file = ~/data/_-_file/by-name/the-effect-of-human-thought-on-data.pdf;
+      url = "/file/the-effect-of-human-thought-on-data.pdf";
+    };
+  };
+  projects = {
     gladiator-manager = {
       file = ~/data/_-_file/by-name/gladiator-manager-1_0_0.zip;
       url = "/file/gladiator-manager.zip";
+    };
+    labyrinth-rl = {
+      file = ~/data/_-_file/by-name/labyrinth-rl-1_0_0.zip;
+      url = "/file/labyrinth-rl.zip";
     };
   };
 }
