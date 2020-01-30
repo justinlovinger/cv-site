25ada33847f3f24a94414138d513ac3a85399f05
Justin Lovinger <git@justinlovinger.com>
2020-01-30 00:12:51 -0500

    Fix dynamicCircles lag

--- src/Component/Dynamiccircles.purs
+++ src/Component/Dynamiccircles.purs
@@ -10,8 +10,9 @@ import Concur.React (HTML)
 import Concur.React.DOM (canvas)
 import Concur.React.Props (_id, height, width)
 import Control.Alt ((<|>))
+import Control.MonadZero (guard)
 import Control.Plus (empty)
-import Data.Array (sortBy, (..))
+import Data.Array ((..))
 import Data.Foldable (foldMap)
 import Data.Int (toNumber)
 import Data.Maybe (Maybe(Just,Nothing), maybe)
@@ -21,11 +22,10 @@ import Effect.Aff (Milliseconds(Milliseconds), delay)
 import Effect.Aff.Class (liftAff)
 import Effect.Class (liftEffect)
 import Effect.Random (random)
-import FRP.Behavior (Behavior, animate, fixB, integral', switcher)
-import FRP.Behavior.Mouse (buttons)
-import FRP.Behavior.Mouse as Mouse
-import FRP.Behavior.Time as Time
-import FRP.Event.Mouse (Mouse, getMouse, down)
+import FRP.Behavior (Behavior, animate, fixB, integral')
+import FRP.Behavior.Mouse (buttons, position)
+import FRP.Behavior.Time (seconds)
+import FRP.Event.Mouse (Mouse, getMouse)
 import Global (infinity)
 import Graphics.Canvas (getCanvasElementById, getContext2D)
 import Graphics.Drawing (Drawing, circle, fillColor, filled, lineWidth, outlineColor, outlined, rectangle, render, scale, translate)
@@ -44,7 +44,7 @@ scene mouse { w, h } = pure background <> map renderCircles circles where
   renderCircle { x, y, size } =
     scale scaleFactor scaleFactor <<< translate x y <<< scale size size $
       outlined
-        (outlineColor (C.altBackground) <> lineWidth ((1.0 + size * 2.0) / scaleFactor))
+        (outlineColor C.altBackground <> lineWidth ((1.0 + size * 2.0) / scaleFactor))
         (circle 0.0 0.0 0.5)
 
   renderCircles ∷ Array Circle → Drawing
@@ -59,31 +59,32 @@ scene mouse { w, h } = pure background <> map renderCircles circles where
   -- So the function exhibits either decay or growth depending on if
   -- the mouse is pressed or not.
   --
-  -- We can solve the differential equation by integration using `solve2'`.
+  -- We can solve the differential equation using an integral and a fixed point.
   swell ∷ Behavior Number
   swell =
       fixB 2.0 \b →
-        integral' 2.0 (unwrap <$> Time.seconds)
-          let db = fixB 10.0 \db_ →
-                     integral' 10.0 (unwrap <$> Time.seconds) (f <$> buttons mouse <*> b <*> db_)
-          in switcher db (down $> db)
+        integral' 2.0 (unwrap <$> seconds) $
+          fixB 0.0 \db →
+            integral' 0.0 (unwrap <$> seconds) $
+              f <$> buttons mouse <*> b <*> db
     where
       f bs s ds | isEmpty bs = -8.0 * (s - 1.0) - ds * 2.0
                 | otherwise = 2.0 * (4.0 - s)
 
   circles ∷ Behavior (Array Circle)
-  circles = toCircles <$> Mouse.position mouse <*> swell where
-    toCircles m sw =
-        sortBy (comparing (\{ x, y } → -(dist x y m))) do
-          i ← 0 .. 16
-          j ← 0 .. 16
-          let x = toNumber i
-              y = toNumber j
-              d = dist x y m
-          pure { x
-               , y
-               , size: 0.1 + (1.0 + sw) / (d + 1.5)
-               }
+  circles = toCircles <$> position mouse <*> swell where
+    toCircles m sw = do
+        i <- 0 .. 16
+        j <- 0 .. 16
+        let x = toNumber i
+            y = toNumber j
+            d = dist x y m
+            size = (1.0 + sw) / (d + 1.5) - 0.2
+        guard $ size > 0.0
+        pure { x
+             , y
+             , size
+             }
       where
         dist x y = maybe infinity \{ x: mx, y: my } →
           let dx = x - toNumber mx / scaleFactor
