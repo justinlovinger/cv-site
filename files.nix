cf6f08fffc4ce37d1d7b1e27c1e7d87edb420eb3
Justin Lovinger <git@justinlovinger.com>
2023-09-19 18:20:45 +0000

    Update file paths

--- files.nix
+++ files.nix
@@ -1,49 +1,49 @@
 {
   publications = {
     auto = {
-      file = ~/data/_file/by-name/auto.pdf;
+      file = ~/_data/_file/by-name/auto.pdf;
       url = "/file/auto.pdf";
     };
     gist = {
-      file = ~/data/_file/by-name/gist.pdf;
+      file = ~/_data/_file/by-name/gist.pdf;
       url = "/file/gist.pdf";
     };
     harnessing-mother-nature = {
-      file = ~/data/_file/by-name/harnessing-mother-nature.pdf;
+      file = ~/_data/_file/by-name/harnessing-mother-nature.pdf;
       url = "/file/harnessing-mother-nature.pdf";
     };
     infinite-lattice-learner = {
-      file = ~/data/_file/by-name/infinite-lattice-learner.pdf;
+      file = ~/_data/_file/by-name/infinite-lattice-learner.pdf;
       url = "/file/infinite-lattice-learner.pdf";
     };
     neural-field = {
-      file = ~/data/_file/by-name/neural-field.pdf;
+      file = ~/_data/_file/by-name/neural-field.pdf;
       url = "/file/neural-field.pdf";
     };
     scrubbing-the-web-for-association-rules = {
-      file = ~/data/_file/by-name/scrubbing-the-web-for-association-rules.pdf;
+      file = ~/_data/_file/by-name/scrubbing-the-web-for-association-rules.pdf;
       url = "/file/scrubbing-the-web-for-association-rules.pdf";
     };
     sma-star-plus = {
-      file = ~/data/_file/by-name/sma-star-plus.pdf;
+      file = ~/_data/_file/by-name/sma-star-plus.pdf;
       url = "/file/sma-star-plus.pdf";
     };
     supervised-learning-as-mathematical-optimization-tutorial = {
-      file = ~/data/_file/by-name/supervised-learning-as-mathematical-optimization-tutorial.pdf;
+      file = ~/_data/_file/by-name/supervised-learning-as-mathematical-optimization-tutorial.pdf;
       url = "/file/supervised-learning-as-mathematical-optimization-tutorial.pdf";
     };
     the-effect-of-human-thought-on-data = {
-      file = ~/data/_file/by-name/the-effect-of-human-thought-on-data.pdf;
+      file = ~/_data/_file/by-name/the-effect-of-human-thought-on-data.pdf;
       url = "/file/the-effect-of-human-thought-on-data.pdf";
     };
   };
   projects = {
     gladiator-manager = {
-      file = ~/data/_file/by-name/gladiator-manager-1_0_0.zip;
+      file = ~/_data/_file/by-name/gladiator-manager-1_0_0.zip;
       url = "/file/gladiator-manager.zip";
     };
     labyrinth-rl = {
-      file = ~/data/_file/by-name/labyrinth-rl-1_0_0.zip;
+      file = ~/_data/_file/by-name/labyrinth-rl-1_0_0.zip;
       url = "/file/labyrinth-rl.zip";
     };
   };
